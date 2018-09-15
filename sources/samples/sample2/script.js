c965a7d52686679331598a8472331680159270b2
Federico Ghedina <fedeghe@gmail.com>
2018-09-16 00:39:47 +0200

    main sample script started

--- sources/samples/sample2/script.js
+++ sources/samples/sample2/script.js
@@ -1,9 +1,59 @@
 var App = (function () {
 
+    var viewF = JMVP.View(),
+        modelF = JMVP.Model(),
+        presenterF = JMVP.Presenter();
+    
+    var modelLogin = modelF({
+            message: 'Enter Your auth info and press login button'
+        }),
+        viewLogin = viewF(`
+            <div>
+                <fieldset class="login">
+                    <legend class="login-legend">Login</legend>
+                    <div>
+                        <label>Username:</label><input type="text"/>
+                    </div>
+                    <div>
+                        <label>Password:</label><input type="password"/>
+                    </div>
+                    <button>login</button><span> or </span><button>skip</button> 
+                    <hr>
+                    <span>$[message]</span>
+                </fieldset>
+            </div>
+        `, modelLogin),
+        
+        modelList = modelF({
+            list: [],
+            loggedIn: false
+        }),
+        viewList = viewF(`<div>Logged in</div>`, modelList),
 
-    return {
-        init : function (trg) {
-            console.log(trg)
+        presenter = presenterF();
+
+
+    var App =  presenter.getSetupsManager({
+        initialize: function () {
+            console.log('Initialize')
+        },
+        login: {
+            view: viewLogin,
+            model: modelLogin,
+            defs: function () {},
+            init: function () {
+                if (this.model.getLoggedIn) {
+                    App.list();
+                }    
+            }
+        },
+        list: {
+            view: viewList,
+            model: modelList,
+            defs: function () {},
+            init: function () {}
         }
-    }
+    });
+
+    return App;
 })();
