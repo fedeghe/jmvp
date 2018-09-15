44dae76f9de11b0e1454e8b31194770451f1be58
Federico Ghedina <fedeghe@gmail.com>
2018-09-15 16:55:50 +0200

    scripts moved out to be used in readme and samples

---
+++ sources/readme/script0.js
@@ -0,0 +1,57 @@
+// Model
+//
+var mFact1 = JMVP.Model(),
+    model = mFact1({
+        name: 'Donald',
+        surname: 'Knuth',
+        email: 'donald.knuth@gmail.com',
+        list: []
+    });
+
+// View
+//
+var vFact1 = JMVP.View(),
+    view = vFact1(`
+    <div>
+        <p>A list of numbers</p>
+        <ul></ul>
+        <p>{name} $[surname] please ...</p>
+        <button>add random</button>
+    </div>
+`);
+
+// bind'em
+view.setModel(model);
+
+// define add Random 
+view.defineMethod('onButtonClickSetHandler', function (handler) {
+    this.setHandler([3], 'click', handler);
+})
+view.defineMethod('addRandom', function (number) {
+    var newNode = document.createElement('li');
+    newNode.appendChild(document.createTextNode(number));
+    this.getNode([0]).appendChild(newNode);
+});
+
+// Presenter
+//
+var pFact1 = JMVP.Presenter(),
+    presenter = pFact1(model, view);
+
+presenter.defineMethod('add', function () {
+    var elements = this.model.getList(),
+        newValue = Math.random().toFixed(3);
+    elements.push(newValue);
+    //update model
+    this.model.setList(elements);
+    //and view
+    this.view.addRandom(newValue);
+})
+
+presenter.init = function () {
+    this.view.onButtonClickSetHandler(this.add);
+};
+
+// finally render
+//
+presenter.render(trg);
---
+++ sources/readme/script2.js
@@ -0,0 +1,64 @@
+var modelF = JMVP.Model(),
+    viewF = JMVP.View(),
+    presenterF = JMVP.Presenter();
+
+var model1 = modelF({
+    title: 'The Cat',
+    description: 'The cat jumps in the lake'
+}),
+    model2 = modelF({ author: 'Federico Ghedina' }),
+    view1 = viewF(`<div>
+            <h1>$[title]</h1>
+            <p>$[description]</p>
+            <a href="#">scramble</a>
+        </div>`, model1), /// oh, sure u can also pass it there as second param
+    view2 = viewF(`<div><span>$[author]</span>&nbsp;<button>back</button></div>`, model2),
+    presenter = presenterF();
+
+
+
+var App = presenter.getSetupsManager({
+    initialize: function () {
+        console.log('Called anyway (context the presenter)')
+    },
+    firstSection: {
+        view: view1,
+        // model: model1,
+        // first define some methods if needed in the presenter
+        defs: function (params) {
+            presenter.defineMethod('scramble', function () {
+                presenter.model.scramble();
+                presenter.view.setDescription(presenter.model.getDescription())
+            });
+            presenter.view.defineMethod('setClickHandler', function (cb) {
+                presenter.view.setHandler([2], 'click', cb);
+            });
+            presenter.view.defineMethod('replaceDescription', function (txt) {
+                presenter.view.getNode([1]).innerHTML = txt;
+            })
+            presenter.model.defineMethod('scramble', function () {
+                var desc = presenter.model.getDescription();
+                presenter.model.setDescription(desc.split('').sort(function () { return Math.random() > 0.5 ? 1 : -1 }).join(''));
+            });
+        },
+
+        init: function (params) {
+            presenter.view.setClickHandler(function () {
+                presenter.model.scramble();
+                presenter.view.replaceDescription(
+                    presenter.model.getDescription()
+                );
+            });
+        }
+    },
+    secondSection: {
+        view: view2,
+        defs: function () {
+
+        },
+        init: function () {
+
+        },
+    }
+});
+App.firstSection({ trg: trg, other: 'params', that: 'You', may: 'need' });
