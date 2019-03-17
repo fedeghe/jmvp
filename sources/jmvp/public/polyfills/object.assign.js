a546c299b5a6dc7468ed3a3ff343959e0dded70d
fedeghe <fedeghe@gmail.com>
2019-03-17 22:45:47 +0100

    some more lint

--- sources/jmvp/public/polyfills/object.assign.js
+++ sources/jmvp/public/polyfills/object.assign.js
@@ -1,19 +1,20 @@
-if (typeof Object.assign != 'function') {
+if (typeof Object.assign !== 'function') {
     // Must be writable: true, enumerable: false, configurable: true
-    Object.defineProperty(Object, "assign", {
-        value: function assign(target, varArgs) { // .length of function is 2
+    Object.defineProperty(Object, 'assign', {
+        value: function assign (target, varArgs) { // .length of function is 2
             'use strict';
             if (target == null) { // TypeError if undefined or null
                 throw new TypeError('Cannot convert undefined or null to object');
             }
 
-            var to = Object(target);
+            var to = Object(target),
+                index, nextSource, nextKey;
 
-            for (var index = 1; index < arguments.length; index++) {
-                var nextSource = arguments[index];
+            for (index = 1; index < arguments.length; index++) {
+                nextSource = arguments[index];
 
                 if (nextSource != null) { // Skip over if undefined or null
-                    for (var nextKey in nextSource) {
+                    for (nextKey in nextSource) {
                         // Avoid bugs when hasOwnProperty is shadowed
                         if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
                             to[nextKey] = nextSource[nextKey];
@@ -26,4 +27,4 @@ if (typeof Object.assign != 'function') {
         writable: true,
         configurable: true
     });
-}
+};
