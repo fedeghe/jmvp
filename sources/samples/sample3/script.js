c91100875fa1b3edb6f65c6fb67c80fe09598471
fedeghe <fedeghe@gmail.com>
2021-03-24 00:19:27 +0100

    bit better

--- sources/samples/sample3/script.js
+++ sources/samples/sample3/script.js
@@ -4,49 +4,62 @@ var mFact = JMVP.Model(),
     model1 = mFact({list: []}),
     view1 = vFact(`<div>
         <button nid="add-button">Create a random number</button>
-        <table></table>
+        <span nid="num"></span>
+        <table nid="table"></table>
     </div>`),
     presenter1 = pFact(model1, view1);
 
 view1.setModel(model1);
-view1.defineMethod('addRandom', function (number) {
-    var model2 = mFact({number: number}),
-        view2 = vFact(`<tr>
-            <td>$[number]</td>
-            <td><button nid="x">-</button></td>
-        </tr>`),
-        pres2 = pFact(model2, view2);
-    
-    view2.setModel(model2);
-    view2.defineMethod('setClickHandler', function (cb){
-        view2.setHandler('x', 'click', cb)
-    });
-    pres2.init = function () {
-        var view = this.view;
-        view.setClickHandler(function () {
-            view.remove()
-        });
-        // or 
-        // view.setClickHandler(view.remove.bind(view));
-    };
-    pres2.render(this.getNode(1));
+
+view1.defineMethod('setAddHandler', function (f) {
+    view1.setHandler(view1.getNode('add-button'), 'click', f)
+});
+view1.defineMethod('removeNode', function ($n, n) {
+    var newList = model1.getList().filter(function (el) {
+            return el !== n
+        }),
+        $num = view1.getNode('num');
+    model1.setList(newList)
+
+    // view1.renderList()
+    // or better
+    $n.parentNode.removeChild($n)
+    $num.innerHTML = newList.length
+});
+view1.defineMethod('renderList', function () {
+    var $table = view1.getNode('table'),
+        $num = view1.getNode('num'),
+        list = model1.getList();
+    $table.innerHTML = '';
+    list.forEach(function (n) {
+        var model = mFact({number: n}),
+            view = vFact(`<tr nid="line">
+                <td>$[number]</td>
+                <td><button nid="remove">-</button></td>
+            </tr>`),
+            presenter = pFact(model, view);
+        view.setModel(model);
+        view.setHandler(view.getNode('remove'), 'click', function () {
+            view1.removeNode(view.node, n)
+        })   
+        presenter.render($table)
+    })
+    $num.innerHTML = list.length;
 });
 
-view1.defineMethod('setAddRandomHandler', function (cb) {
-    view1.setHandler('add-button', 'click', cb);
-});
 
 presenter1.init = function () {
-    var self = this;
-    this.defineMethod('addNumber', function (n) {
-        var list = self.model.getList();
-        list.push(n);
-        self.model.setList(list);
-        self.view.addRandom(n);
-    });
-    this.view.setAddRandomHandler(function () {
-        self.addNumber(Math.random().toFixed(3));
-    });
+    var self = this,
+        view = self.view,
+        model = self.model;
+
+    view.setAddHandler(function () {
+        var list = model.getList();
+        list.push(Math.random().toFixed(3))
+        model.setList(list);
+        view.renderList()
+    })
+
 };
 
 presenter1.render(document.getElementById('trg'));
